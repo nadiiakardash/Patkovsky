98b78c4218304a0b95454560151edad6128f9d56
kiforks <ks.kifor@gmail.com>
2020-10-09 17:24:02 +0300

    Fixed

--- uncompressed/js/scripts/init/aos-init.js
+++ uncompressed/js/scripts/init/aos-init.js
@@ -1,11 +1,4 @@
-if (document.documentElement.clientWidth < 1440) {
-  AOS.init({
-    duration: 900,
-    offset: 0
-  })
-} else {
-  AOS.init({
-    duration: 900,
-    offset: 100
-  })
-}
+AOS.init({
+  duration: 900,
+  offset: 0
+})
